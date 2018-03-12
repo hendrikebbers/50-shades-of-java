3a140f9f69a54d9bf6e6ffc6a0926626c33754fe
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-03-12 16:49:51 +0100

    self-destruction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Main {
 
     public static void main(String[] args) {
-        $.killOut();
-        System.out.println("Hello World!");
+        // -XX:SelfDestructTimer=1
+        LocalTime startTime = LocalTime.now();
+        while (true) {
+            System.out.println("Up " + Duration.between(startTime, LocalTime.now()).getSeconds() + " s");
+            $.sleep(2_000);
+        }
     }
 
 }

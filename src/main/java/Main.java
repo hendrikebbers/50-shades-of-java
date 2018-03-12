f33efb6ffb3737fbff909187bc0dc3f40442e5b0
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-03-12 13:52:27 +0100

    Hide Exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        breaker();
-    }
-
-    public static void gotoLabel() {
-        gotoJava:
-        for (int i = 0; i < 10; i++) {
-            innerGotoJava:
-            for (int j = 0; j < 10; j++) {
-                System.out.println(j + "");
-                if(j > 5) {
-                    continue gotoJava;
-                    //   break innerGotoJava;
-                }
-            }
+        try {
+            System.out.println("1");
+            throw new Exception("ARGH!");
+        } finally {
+            System.out.println("2");
+            return;
         }
-
-        http://www.guigarage.com
-        System.out.println("");
     }
 
-
-    public static void testSomething() {
-        int i = 0;
-
-        given:
-            i = 1;
-
-        when:
-            i ++;
-
-        then:
-            assert i == 1;
-    }
-
-    public static void breaker() {
-        breakable: {
-            System.out.println(1 + "");
-            System.out.println(2 + "");
-            if(true) {
-                break breakable;
-            }
-            System.out.println(3 + "");
-        }
-        System.out.println(4 + "");
-    }
 }

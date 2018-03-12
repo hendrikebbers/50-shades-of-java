73b80fbdec32be024181df95cd231501808380a4
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-03-12 13:46:23 +0100

    Labels

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,49 @@
 public class Main {
 
     public static void main(String[] args) {
-        $.list("Java", "JavaScript").forEach(s -> $.print(s));
+        breaker();
     }
 
+    public static void gotoLabel() {
+        gotoJava:
+        for (int i = 0; i < 10; i++) {
+            innerGotoJava:
+            for (int j = 0; j < 10; j++) {
+                System.out.println(j + "");
+                if(j > 5) {
+                    continue gotoJava;
+                    //   break innerGotoJava;
+                }
+            }
+        }
+
+        http://www.guigarage.com
+        System.out.println("");
+    }
+
+
+    public static void testSomething() {
+        int i = 0;
+
+        given:
+            i = 1;
+
+        when:
+            i ++;
+
+        then:
+            assert i == 1;
+    }
+
+    public static void breaker() {
+        breakable: {
+            System.out.println(1 + "");
+            System.out.println(2 + "");
+            if(true) {
+                break breakable;
+            }
+            System.out.println(3 + "");
+        }
+        System.out.println(4 + "");
+    }
 }
